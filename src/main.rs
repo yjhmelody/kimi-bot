ec2849a934be5b86dc6670f640171b8836756b70
yjhmelody <yjh465402634@gmail.com>
2024-08-28 20:36:02 +0800

    feat: add show/update config cmds

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,18 @@ async fn main() {
 
     let openai_key = env::var("OPENAI_API_KEY").expect("Missing OPENAI_API_KEY env variable");
     let openai_url = env::var("OPENAI_API_URL").expect("Missing OPENAI_API_URL env variable");
+    let openai_model = env::var("OPENAI_MODEL").unwrap_or("moonshot-v1-8k".to_string());
 
-    let config = OpenAIConfig::new()
-        .with_api_key(openai_key)
-        .with_api_base(openai_url)
-        .with_org_id("the-continental");
-    let client = Client::with_config(config);
+    let config = Config {
+        openai_key,
+        openai_model,
+        openai_url,
+    };
 
-    let state = State::new_shared(client);
+    let openai_config = config.to_openai_config();
+    let client = Client::with_config(openai_config);
+
+    let state = State::new_shared(State::new(client).with_config(config));
     let bot = Bot::from_env();
 
     let ignore_update = |_upd| Box::pin(async {});
@@ -52,28 +56,58 @@ pub type SharedState = Arc<RwLock<State>>;
 pub struct State {
     messages_total: u64,
     client: Client<OpenAIConfig>,
+    config: Config,
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct Config {
+    openai_key: String,
+    openai_url: String,
+    openai_model: String,
+}
+
+impl Config {
+    pub fn to_openai_config(&self) -> OpenAIConfig {
+        let config = OpenAIConfig::new()
+            .with_api_key(self.openai_key.clone())
+            .with_api_base(self.openai_url.clone())
+            .with_org_id("the-continental");
+
+        config
+    }
 }
 
 impl State {
-    pub fn new_shared(client: Client<OpenAIConfig>) -> SharedState {
-        Arc::new(RwLock::new(State::new(client)))
+    pub fn new_shared(state: Self) -> SharedState {
+        Arc::new(RwLock::new(state))
     }
 
     pub fn new(client: Client<OpenAIConfig>) -> Self {
         Self {
-            messages_total: 0,
             client,
+            ..Self::default()
         }
+    }
+
+    pub fn with_config(mut self, config: Config) -> Self {
+        self.config = config;
+        self
     }
 }
 
 #[derive(BotCommands, Clone)]
-#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+#[command(rename_rule = "kebab-case", description = "These commands are supported:")]
 enum Command {
     #[command(description = "display this text.")]
     Help,
+    #[command(description = "Show current config")]
+    CurrentConfig,
     #[command(description = "chat with kimi.")]
     Kimi(String),
+    #[command(description = "Update the endpoint config of kimi")]
+    UpdateEndpoint(String),
+    #[command(description = "Update the model config of kimi")]
+    UpdateModel(String),
 }
 
 async fn answer(bot: Bot, state: SharedState, cmd: Command, msg: Message) -> ResponseResult<()> {
@@ -85,10 +119,21 @@ async fn answer(bot: Bot, state: SharedState, cmd: Command, msg: Message) -> Res
     let f = async {
         match cmd {
             Command::Help => bot.send_message(id, Command::descriptions().to_string()).await?,
+            Command::CurrentConfig => {
+                let response = {
+                    let state = state.read().await;
+                    let config = &state.config;
+                    format!("config: url({}), model({})", config.openai_url, config.openai_model)
+                };
+                bot.send_message(id, response).await?
+            },
             Command::Kimi(text) => {
+                let model = {
+                    let state = state.read().await;
+                    state.config.openai_model.clone()
+                };
                 let request = CreateChatCompletionRequestArgs::default()
-                    // must use moonshot model
-                    .model("moonshot-v1-8k")
+                    .model(model)
                     .messages([ChatCompletionRequestMessage::User(ChatCompletionRequestUserMessage {
                         content: text.into(),
                         ..Default::default()
@@ -107,6 +152,28 @@ async fn answer(bot: Bot, state: SharedState, cmd: Command, msg: Message) -> Res
                     .clone()
                     .ok_or(unknown_error("The remote chat AI response failed"))?;
                 bot.send_message(id, reply).await?
+            }
+            Command::UpdateEndpoint(endpiont) => {
+                {
+                    let mut state = state.write().await;
+                    state.config.openai_url = endpiont;
+                    // create a new config
+                    let openai_config = state.config.to_openai_config();
+                    state.client = Client::with_config(openai_config);
+                    log::debug!("The config: {:#?}", state.config);
+                }
+                bot.send_message(id, "Update endpoint successful.").await?
+            }
+            Command::UpdateModel(model) => {
+                {
+                    let mut state = state.write().await;
+                    state.config.openai_model = model;
+                    // create a new config
+                    let openai_config = state.config.to_openai_config();
+                    state.client = Client::with_config(openai_config);
+                    log::debug!("The config: {:#?}", state.config);
+                }
+                bot.send_message(id, "Update model successful.").await?
             }
         };
 
